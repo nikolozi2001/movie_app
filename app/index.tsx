3ce03b096c8d90550359b05967129579b7da95b4
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-09-11 15:36:32 +0400

    Initial commit: Catch the Food app with NativeWind setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500 text-3xl">
+        Welcome to Catch The Food!
+      </Text>
+    </View>
+  );
+}
